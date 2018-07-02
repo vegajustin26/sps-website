cab8ed70d89d6990f240cfee0d0fec6fad80ef9b
Nabeel Sherazi <sherazi.n@husky.neu.edu>
2018-07-02 01:19:23 -0400

    revert jquery to the old way we were doing it

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -3,16 +3,12 @@ $(window).scroll(function() {
     var nav = $("nav"); // Cache as variable
 
     if ( $(window).scrollTop() > 0 && nav.attr("nav-type") == "expanded" ) {
-        // $(".navbar-text").fadeOut(500);
-        $(".navbar-text").fadeTo("slow", 0, function() {
-            $(this).addClass("hide");
-        });
+        $(".navbar-text").fadeOut(500);
         nav.attr("nav-type", "mini");
     }
 
     else if ( $(window).scrollTop() == 0 && nav.attr("nav-type") == "mini" ) {
         nav.attr("nav-type", "expanded");
-        // $(".navbar-text").fadeIn(500);
-        $(".navbar-text").removeClass("hide").fadeTo("slow", 1);
+        $(".navbar-text").fadeIn(500);
     }
 })
